dfaeba3ca6f9ac9d7c2e7d1beb1bcbf58b3b217e
RomaKorol-Js <rr6456775@gmail.com>
2026-06-02 09:38:52 +0000

    feat: compleat and test llab 9, add update to Post, and add new model data for BlogPost with relationship to User and Category with data from this models visible in request

--- app/Http/Controllers/Api/Blog/Admin/PostController.php
+++ app/Http/Controllers/Api/Blog/Admin/PostController.php
@@ -4,6 +4,10 @@ namespace App\Http\Controllers\Api\Blog\Admin;
 
 
 use App\Repositories\BlogPostRepository;
+use App\Repositories\BlogCategoryRepository;
+use App\Http\Requests\BlogPostUpdateRequest;
+use Carbon\Carbon;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 
 class PostController extends BaseController
@@ -41,9 +45,33 @@ class PostController extends BaseController
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(BlogPostUpdateRequest  $blogPostRepository, string $id)
     {
-        //
+     $item = $this->blogPostRepository->getEdit($id);
+        if (empty($item)) { //якщо ід не знайдено
+            return ['message' => "Запис id=[{$id}] не знайдено"];
+        }
+
+        $data = $blogPostRepository->all(); //отримаємо масив даних, які надійшли з форми
+        
+        if (empty($data['slug'])) { //якщо псевдонім порожній
+            $data['slug'] = Str::slug($data['title']); //генеруємо псевдонім
+        }
+        if (empty($item->published_at) && $data['is_published']) { //якщо поле published_at порожнє і нам прийшло 1 в ключі is_published, то
+            $data['published_at'] = Carbon::now(); //генеруємо поточну дату
+        }        
+        $result = $item->update($data); //оновлюємо дані об'єкта і зберігаємо в БД
+
+        if ($result) {
+            return [
+            'data' => $data,
+            'success' => true,
+            'message' => 'Успішно збережено'
+        ];
+        } else {
+            return ['message' => 'Помилка збереження'];
+        }
+ 
     }
 
     /**
