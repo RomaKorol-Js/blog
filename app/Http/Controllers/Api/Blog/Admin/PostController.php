05688f925f1b7180c6d78f398aaa762bb83e870c
RomaKorol-Js <rr6456775@gmail.com>
2026-06-06 14:19:20 +0000

    feat:lab 10 done

--- app/Http/Controllers/Api/Blog/Admin/PostController.php
+++ app/Http/Controllers/Api/Blog/Admin/PostController.php
@@ -6,7 +6,8 @@ namespace App\Http\Controllers\Api\Blog\Admin;
 use App\Repositories\BlogPostRepository;
 use App\Repositories\BlogCategoryRepository;
 use App\Http\Requests\BlogPostUpdateRequest;
-use Carbon\Carbon;
+
+// use Carbon\Carbon;
 use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 
@@ -54,12 +55,12 @@ class PostController extends BaseController
 
         $data = $blogPostRepository->all(); //отримаємо масив даних, які надійшли з форми
         
-        if (empty($data['slug'])) { //якщо псевдонім порожній
-            $data['slug'] = Str::slug($data['title']); //генеруємо псевдонім
-        }
-        if (empty($item->published_at) && $data['is_published']) { //якщо поле published_at порожнє і нам прийшло 1 в ключі is_published, то
-            $data['published_at'] = Carbon::now(); //генеруємо поточну дату
-        }        
+        // if (empty($data['slug'])) { //якщо псевдонім порожній
+        //     $data['slug'] = Str::slug($data['title']); //генеруємо псевдонім
+        // }
+        // if (empty($item->published_at) && $data['is_published']) { //якщо поле published_at порожнє і нам прийшло 1 в ключі is_published, то
+        //     $data['published_at'] = Carbon::now(); //генеруємо поточну дату
+        // }        
         $result = $item->update($data); //оновлюємо дані об'єкта і зберігаємо в БД
 
         if ($result) {
